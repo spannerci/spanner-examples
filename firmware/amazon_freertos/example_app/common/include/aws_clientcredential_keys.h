2c24e7fdc3e18da1c63c81a561be2f06cd263d59
cpipilas <30462769+cpipilas@users.noreply.github.com>
2019-02-24 16:07:13 +0200

    Update aws_clientcredential_keys.h

--- firmware/amazon_freertos/example_app/common/include/aws_clientcredential_keys.h
+++ firmware/amazon_freertos/example_app/common/include/aws_clientcredential_keys.h
@@ -38,24 +38,7 @@
  */
 #define keyCLIENT_CERTIFICATE_PEM \
 "-----BEGIN CERTIFICATE-----\n"\
-"MIIDWTCCAkGgAwIBAgIUGdpbxGUksGwG7nGt5i1nLhz//+8wDQYJKoZIhvcNAQEL\n"\
-"BQAwTTFLMEkGA1UECwxCQW1hem9uIFdlYiBTZXJ2aWNlcyBPPUFtYXpvbi5jb20g\n"\
-"SW5jLiBMPVNlYXR0bGUgU1Q9V2FzaGluZ3RvbiBDPVVTMB4XDTE5MDIxMzExMTkz\n"\
-"NloXDTQ5MTIzMTIzNTk1OVowHjEcMBoGA1UEAwwTQVdTIElvVCBDZXJ0aWZpY2F0\n"\
-"ZTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKnhsrpPTJBWVRGZ27TS\n"\
-"uDrHmb9IsRWnOM14DtC4ecSSXVEozCCl1YpmUms+ZSBjdAeOfCsONJcn8eqikxoM\n"\
-"VSDe6kMnzAG+flIlpaXrUqX5Aed9/I1gosm4dDsWppKzIQXZN/ihnTvYM6FxanbE\n"\
-"faRucBy4xMcumMyCThqrEwj/vbe3cZjVNzj3JvaWOtHcDAIdlg/mF4+pPrKhZnHt\n"\
-"GcygHahsvjyONdFS9TZuGXKVQr/VDxX6jauURxg393Yv9v7xS6CRx7Ki1plPADmk\n"\
-"h/ZBdjqYRPaGnvcvJf0lRwqeLK4RTEWAPLh02Jj5dnxRQBIQlcTviZ3bpwlaVz1y\n"\
-"xLsCAwEAAaNgMF4wHwYDVR0jBBgwFoAU6ibL//+F2DPU7coC/BZEoLJE/3AwHQYD\n"\
-"VR0OBBYEFLA7/KWTw93cIirWU18K0jSSAsBbMAwGA1UdEwEB/wQCMAAwDgYDVR0P\n"\
-"AQH/BAQDAgeAMA0GCSqGSIb3DQEBCwUAA4IBAQCT3YUp85nkB02JTZLBpYt5ecRa\n"\
-"DxipoqAG/UfOl8zo2jA9sROz/xclXsxknW/nZoepufL7PEjZkQ3pFV7TDrrsXLlK\n"\
-"shJ160Ju6s3HTmywXdwiIjRwD9g+ZHn65EHAqTZSbIdVm289qLhDRxjCye9XBCCt\n"\
-"3wd+LdfJu1IGzEyQZVPN0D7tVoAcft33BDexp7logQf0Iq91Z1MsosNPZs+xKT9V\n"\
-"IrMbYkaEehVR6ksaOTlnmDS6kolwRIP1GT7mRO3/pH9dVs+8UerBJas4szA4mji9\n"\
-"thFgoDkS2X8hbsS2oiYsIl6LPKJYVD78fvmIGvEVuErGicCst28SSbHZokhR\n"\
+"...paste client cert here... \n"\
 "-----END CERTIFICATE-----\n"
 
 /*
@@ -86,32 +69,8 @@
  */
 #define keyCLIENT_PRIVATE_KEY_PEM \
 "-----BEGIN RSA PRIVATE KEY-----\n"\
-"MIIEowIBAAKCAQEAqeGyuk9MkFZVEZnbtNK4OseZv0ixFac4zXgO0Lh5xJJdUSjM\n"\
-"IKXVimZSaz5lIGN0B458Kw40lyfx6qKTGgxVIN7qQyfMAb5+UiWlpetSpfkB5338\n"\
-"jWCiybh0OxamkrMhBdk3+KGdO9gzoXFqdsR9pG5wHLjExy6YzIJOGqsTCP+9t7dx\n"\
-"mNU3OPcm9pY60dwMAh2WD+YXj6k+sqFmce0ZzKAdqGy+PI410VL1Nm4ZcpVCv9UP\n"\
-"FfqNq5RHGDf3di/2/vFLoJHHsqLWmU8AOaSH9kF2OphE9oae9y8l/SVHCp4srhFM\n"\
-"RYA8uHTYmPl2fFFAEhCVxO+JndunCVpXPXLEuwIDAQABAoIBAQCcktO62em0Qo3H\n"\
-"WipgX7LDIkJ3RdVJIsW8woHNJ6m7Xjc53UM+d/n3xFUEWQ61qlQ/vrh8qmwYQQ27\n"\
-"9zljUaiIUkxRtvVVSjB9IAurf9e992Ik7T5/Q2jmSOZ/q2G9ZfHyxQDMXJBhnuP/\n"\
-"UVwRuQogKlWUc0uSVUUpgJPiCnjR2fshT4I9th7w00X0ob7Ffyj8tH9RZ4/YmpkZ\n"\
-"nr+lH1d3ERC+yQkw2Tq0sb9KVObkBJyReXKDkaWn5dxU2D217Qm4hsqrDBkpf7YA\n"\
-"XCirhP+P/2Dyle6whSyYFTx2NWd5Y9QXZV9pffEnbV/vOG3/ydOPyI2tAveyuX9F\n"\
-"BUnlFfBxAoGBANuT44RfG1awB052X6WbOZJoQJXCMBjmhI+9J/S8PysAYvIzZ047\n"\
-"LST3MBKqphGSLRyyRa7k1yBq7SYwgpdNYJ8ybuzMjZhKdlOOkQgzEuH5RtjwCH3s\n"\
-"V2gcgYHYTyIgryl+pVyk+g7qYzlNfKoVxPfjNSXYciMm7yLS3apAydTpAoGBAMYP\n"\
-"hi7aY/Y2qf8yLS9gfc3V+Lxh8LsdpkfGVWJrNtzBChQ8lQGz1dSDXrLf2XhTKZYJ\n"\
-"321KyqGb5RX3smGPwcLS4InFM1np3hlMbvkQvN+aMOXDvrf4l50F/lAh5VBmCzcz\n"\
-"IMoRlckVIxCtSngItwf9GagBaCjdFbqOnSDDt1YDAoGAcwVDFyIi1gQbr7Q+Yiev\n"\
-"vsuxmUFBpKmMIyd4+LBrfoETU8Ati0NgYI7DteOCXo3t56xS6EyjjC220Fx07ADy\n"\
-"bjIi/RQhz/ahiR+TRXJLn+fOfvEt4ZieKMFV7ch7m2r3lorq4F7jRlDtbSTz+ryw\n"\
-"kxGH061QDK3mcp121CpgD5ECgYAWxxhGcSVH3peEWs/pf+5X6sC1YYoe6tUBsiSF\n"\
-"shDYKHcxRppYXMc9rnIInkfTV7UTpUUFu7E8DsK02Qyxvaep8TiTf5NdRtEdFbY5\n"\
-"fJxMFt6CvhVD8Rn2nAbYTl2h9mpZyc5+jrEsM/sZYuSKk24BRDFcKfVBpdNm5hnZ\n"\
-"98gw4wKBgA/xqm6Du0rIQ0CV8KjMCAAhB7RxyLHfZFAKQVrzpu54Qk0pgcAbVkCk\n"\
-"HRgvT8bJFfvCx8Je8voI6q5Oolrh8n7Pbft1cmw4Fuyd3ypHxBSaLHB0Gx3eimjM\n"\
-"SZ1PGyMKqeIcurPLwNn+2CO2Rips62RUs3wAkJyEya6KArfvuYw+\n"\
-"-----END RSA PRIVATE KEY-----\n"
+"...paste private key here...\n"\
+ "-----END RSA PRIVATE KEY-----\n"
 
 /* The constants above are set to const char * pointers defined in aws_demo_runner.c,
  * and externed here for use in C files.  NOTE!  THIS IS DONE FOR CONVENIENCE
